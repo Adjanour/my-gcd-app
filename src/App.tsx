088ca1161e1e9e74490560f2c74aef8468b4f1c8
Adjanour <adjanour@icloud.com>
2024-04-11 19:43:43 +0000

    added files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import GCDCalculator from './components/GCDCalculator';
 import './App.css';
-import StepDisplay from './components/StepDisplay';
+
 
 const App: React.FC = () => {
   return (
--- src/components/GCDCalculator.tsx
+++ src/components/GCDCalculator.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import StepDisplay from "./StepDisplay";
+
 
 interface GCDCalculatorProps {}
 
